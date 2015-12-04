6ee456b4aae46780d2c7214bbbe9025bd221f9a1
Matthias Steiner <matthias.steiner@sap.com>
2015-12-04 17:36:18 +0100

    Initial
    Change-Id: Iab93226bd897e3e523a653f03a06990a1e785dfc
    Signed-off-by: Matthias Steiner <matthias.steiner@sap.com>

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # simple-sample-servlet30
+
 Simple sample app demonstrating basic features of Servlet 3.0 
---
+++ src/main/java/test/TestServlet.java
@@ -0,0 +1,141 @@
+package test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+import java.text.MessageFormat;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+@WebServlet(urlPatterns = "/TestServlet", loadOnStartup = 1)
+public class TestServlet extends HttpServlet
+{
+	/**
+	 * 
+	 */
+    private static final long serialVersionUID = 1L;
+
+    @Resource
+    private javax.sql.DataSource ds;
+    
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+	        IOException
+	{
+		PrintWriter writer = response.getWriter();
+		writer.println("A servlet without web.xml: " + getServletName());
+		
+		if (ds != null)
+		{
+			try
+			{
+				DatabaseMetaData metaData = ds.getConnection().getMetaData();
+				writer.println(new DBInformation(metaData).toString());
+			}
+			catch (Exception ex)
+			{
+				ex.printStackTrace(writer);
+			}
+			
+		}
+	}
+	
+	/**
+	 * Provides meta information about the underlying DB of the bound {@link DataSource}. 
+	 * 
+	 * @see DataSource#getConnection()
+	 * @see Connection#getMetaData()
+	 */
+	public static class DBInformation
+	{
+		String url = null;
+
+		String dbName = null;
+		int dbMajorVersion = 0;
+		int dbMinorVersion = 0;
+
+		String driverName = null;
+		String driverVersion = null;
+
+		String userName = null;
+
+		public DBInformation(DatabaseMetaData metaData) throws SQLException
+		{
+			url = metaData.getURL();
+			dbName = metaData.getDatabaseProductName();
+			dbMajorVersion = metaData.getDatabaseMajorVersion();
+			dbMinorVersion = metaData.getDatabaseMinorVersion();
+
+			driverName = metaData.getDriverName();
+			driverVersion = metaData.getDriverVersion();
+
+			userName = metaData.getUserName();
+		}
+
+		public String getUrl()
+		{
+			return this.url;
+		}
+
+		public String getDbName()
+		{
+			return this.dbName;
+		}
+
+		public int getDbMajorVersion()
+		{
+			return this.dbMajorVersion;
+		}
+
+		public int getDbMinorVersion()
+		{
+			return this.dbMinorVersion;
+		}
+
+		public String getDriverName()
+		{
+			return this.driverName;
+		}
+
+		public String getDriverVersion()
+		{
+			return driverVersion;
+		}
+
+		public String getUserName()
+		{
+			return userName;
+		}
+		
+		public String toString()
+		{
+			final String CRLF = System.getProperty("line.separator");
+			
+			StringWriter str = new StringWriter();
+			
+			str.write(CRLF);
+			str.write(MessageFormat.format("Driver Name: {0}{1}", this.driverName, CRLF));
+			str.write(MessageFormat.format("Driver Version: {0}{1}", this.driverVersion, CRLF));
+			str.write(MessageFormat.format("URL: {0}{1}", this.url, CRLF));
+			str.write(MessageFormat.format("DB Name: {0}{1}", this.dbName, CRLF));
+			str.write(MessageFormat.format("Username: {0}{1}", this.userName, CRLF));	
+		
+			return str.toString();
+		}
+		
+	}
+}
